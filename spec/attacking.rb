16e8f2fcdcd36471c4bdc8eab993fafff9cd68e3
Ted Swedalla <fluffingthevoid@gmail.com>
2018-02-07 00:15:40 +0000

    automatically backed up by learn

--- spec/attacking.rb
+++ spec/attacking.rb
@@ -30,9 +30,9 @@ def profbonus(level,attack,damage,ability,moddamage,totaldamage)
     elsif ability >=11
       modattack =1
     end
-    #fullattack = modattack + levelattack
-  #  puts "Total Attack roll #{fullattack}"
-    #hitmonster?(attack,fullattack,damage,modattack,moddamage,totaldamage)
+    fullattack = modattack + levelattack
+    puts "Total Attack roll #{fullattack}"
+    hitmonster?(attack,fullattack,damage,modattack,moddamage,totaldamage)
   end
 
 
