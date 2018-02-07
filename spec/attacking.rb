bb697c6be8e50984ffa3030fed0c3a10224c89be
Ted Swedalla <fluffingthevoid@gmail.com>
2018-02-07 00:55:06 +0000

    automatically backed up by learn

--- spec/attacking.rb
+++ spec/attacking.rb
@@ -56,11 +56,11 @@ def profbonus(level,attack,damage,ability,moddamage,totaldamage)
   #  end
     #fullattack = modattack + attack
     puts "Total Attack roll #{attack}"
-    hitmonster?(attack,damage,modattack,moddamage,totaldamage)
+    hitmonster?(attack,damage,moddamage,totaldamage)
   end
 
 
-def hitmonster?(attack,damage,modattack,moddamage,totaldamage)
+def hitmonster?(attack,damage,moddamage,totaldamage)
   if critorfail?(attack)
     moddamage = (damage*2) + modattack
       puts "CRIT!! You hit for #{moddamage} damage!"
